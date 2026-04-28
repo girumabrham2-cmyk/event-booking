cd24d48de363d00ab32363ff5e04ad579139dc9e
girumabrham2@gmail.com <girumabrham2@gmail.com>
2026-04-28 23:12:32 +0300

    front end folder added

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch("https://event-booking-udyx.onrender.com/events")
+      .then(res => res.json())
+      .then(data => setEvents(data));
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Event Booking</h1>
+
+      {events.map(event => (
+        <div key={event._id} style={{ border: "1px solid #ccc", margin: 10 }}>
+          <h3>{event.title}</h3>
+          <p>{event.date}</p>
+          <p>Price: ${event.price}</p>
+          <button>Book Now</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
